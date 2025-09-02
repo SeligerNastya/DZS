214b02baffc8cc2b1d70381a56e4876dc47e6bf1
SeligerNastya <SeligerNastya@yandex.ru>
2025-09-02 16:11:15 +0300

    Добавлена настройки css для меню

--- seligerbereg/base/us/admin.py
+++ seligerbereg/base/us/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Us, Home
+from .models import Us, Home, Attractions, Gallery
 
 admin.site.register(Us)
 admin.site.register(Home)
+admin.site.register(Attractions)
+admin.site.register(Gallery)
--- seligerbereg/base/us/models.py
+++ seligerbereg/base/us/models.py
@@ -10,3 +10,11 @@ class Home(models.Model):
     description = models.TextField(null=True, blank=True)
     price = models.CharField(blank=True)
     featured_images = models.ImageField(upload_to="home/%Y/%m/%d/", blank=True)
+class Attractions(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    featured_images = models.ImageField(upload_to="home/%Y/%m/%d/", blank=True)
+
+class Gallery(models.Model):
+    image = models.ImageField(upload_to='gallery')
+    attractions = models.ForeignKey(Attractions, on_delete=models.CASCADE, related_name='images')
